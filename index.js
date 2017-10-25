7a83621b87a085d6b8c2efd19b9d8d90903395bc
theuygun <31635234+theuygun@users.noreply.github.com>
2017-10-25 15:09:31 +0300

    Adding select files

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const database = require('./database');
-var input = document.getElementById("myText").value;
+const input = document.getElementById("myText").value;
+var text = "";
+setInterval(function(){
+	$.getScript("db.json", function(){
+	if(text != db["db"][db.db.length - 1]["text"]){
+	var input = document.getElementById ("text_area_2");
+	input.selectionStart = db["db"][db.db.length - 1]["char"];
+	input.selectionEnd = db["db"][db.db.length - 1]["char"];
+	insertAtCaret('text_area_2', db["db"][db.db.length - 1]["text"]);
+	text = db["db"][db.db.length - 1]["text"];
+	}
+});
+}, 100);
 input.addEventListener("keydown", function(evt){
     let Char = String.fromCharCode(evt.keyCode);
     let SeStart = input.selectionStart;
